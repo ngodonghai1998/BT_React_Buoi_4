76eb4cfc86806afc3d1e9e863b3a120b0ed64632
ngodonghai1998 <haiscar17@gmail.com>
2023-05-20 18:06:23 +0700

    laptop 07

--- src/ReactForm/CreateForm.jsx
+++ src/ReactForm/CreateForm.jsx
@@ -1,59 +1,141 @@
 import React, { Component } from 'react'
 
 export default class CreateForm extends Component {
+
+    state = {
+        values: {
+            sinhVienId: '',
+            sinhVienName: '',
+            sinhVienPhone: '',
+            sinhVienEmail: '',
+        },
+        errors: {
+            sinhVienId: '(*)',
+            sinhVienName: '(*)',
+            sinhVienPhone: '(*)',
+            sinhVienEmail: '(*)',
+        },
+    }
+
+    validSinhVienSubmit = (valid) => {
+        // Ngăn không cho button loại submit chạy mặc định.
+        valid.preventDefault();
+
+        // Kiểm tra dữ liệu.
+        for (let key in this.state.errors) {
+            if (this.state.errors[key] !== '') {
+                alert('Dữ liệu không hợp lệ');
+                return;
+            }
+        }
+
+        // Thêm sinh viên vô table.
+        let {addSinhVien} = this.props;
+        addSinhVien(this.state.values);
+
+    }
+
+    validSinhVienChange = (valid) => {
+        // Thẻ target xử lý sự kiện cho onInput ở dưới.
+        let {value, id} = valid.target;
+        let dataType = valid.target.getAttribute('data-type');
+
+        // Xử lý this.state.values.
+        let newValue = {...this.state.values};
+        newValue[id] = value;
+
+        // Xử lý this.state.errors.
+        let newError = {...this.state.errors};
+        let messError = '';
+        if (value.trim() === '') {
+            messError = id + 'Không được bỏ trống!'
+        } else {
+            if (dataType) {
+                switch(dataType) {
+                    case 'number': {
+                        let regexNumber = /^\d+$/;
+                        if (!regexNumber.test(value)) {
+                            messError = id + ' phải là số !';
+                        };
+                        break;
+                    }
+                    case 'string': {
+                        let regexNumber = /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/;
+                        // let regexNumber = /^[a-z A-Z0-9]+$/;
+                        if (!regexNumber.test(value)) {
+                            messError = id + ' phải là ký tự !';
+                        };
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        }
+        newError[id] = messError;
+
+        // Cập nhật lại state sau khi xử lý.
+        this.setState({
+            values: newValue,
+            errors: newError,
+        }, () => {
+            console.log(this.state);
+        })
+    }
+
+    //Chỉ chạy khi props thay đổi và trước khi render (thường dùng cho việc gán props và state)
+    componentWillReceiveProps(newProps) {
+        this.setState({
+            values: newProps.editSinhVien,
+        })
+    }
+
     render() {
+
+        let {sinhVienId, sinhVienName, sinhVienPhone, sinhVienEmail} = this.state.values;
+
         return (
-            <form className='card'>
+            <form className='card' onSubmit={this.validSinhVienSubmit}>
                 <div className='card-header bg-dark text-white'>Product Info</div>
                 <div className='card-body'>
                     <div className='row'>
                         <div className='col-6'>
                             <div className='form-group'>
-                                <p>Id</p>
-                                <input data-type='number' className='form-control' id='idProduct' name='idProduct' />
-                                <p className='text text-danger' ></p>
+                                <p>Mã SV</p>
+                                <input data-type='number' className='form-control' id='sinhVienId' name='sinhVienId'  onInput={this.validSinhVienChange} value={sinhVienId}/>
+                                <p className='text text-danger' >{this.state.errors.sinhVienId}</p>
                             </div>
 
                             <div className='form-group'>
-                                <p>name</p>
-                                <input data-type='string' className='form-control' id='name' name='name' />
-                                <p className='text text-danger' ></p>
-                            </div>
-
-                            <div className='form-group'>
-                                <p>price</p>
-                                <input data-type='number' className='form-control' id='price' name='price' />
-                                <p className='text text-danger' ></p>
+                                <p>Số điện thoại</p>
+                                <input className='form-control' id='sinhVienPhone' name='sinhVienPhone' value={sinhVienPhone} onInput={this.validSinhVienChange}/>
+                                <p className='text text-danger' >{this.state.errors.sinhVienPhone}</p>
                             </div>
 
                         </div>
                         <div className='col-6'>
                             <div className='form-group'>
-                                <p>img</p>
-                                <input className='form-control' id='img' name='img' />
-                                <p className='text text-danger' ></p>
+                                <p>Họ tên</p>
+                                <input data-type='string' className='form-control' id='sinhVienName' name='sinhVienName' value={sinhVienName} onInput={this.validSinhVienChange}/>
+                                <p className='text text-danger' >{this.state.errors.sinhVienName}</p>
                             </div>
 
                             <div className='form-group'>
-                                <p>type</p>
-                                <select className='form-control' id='type' name='type' >
-                                    <option value="phone">Phone</option>
-                                    <option value="tablet">Tablet</option>
-                                    <option value="laptop">Laptop</option>
-                                </select>
-                            </div>
-
-                            <div className='form-group'>
-                                <p>desc</p>
-                                <input data-minlength='6' data-maxlength='32' className='form-control' id='desc' name='desc' />
-                                <p className='text text-danger' ></p>
+                                <p>Email</p>
+                                <input data-minlength='6' data-maxlength='32' datatype='' className='form-control' id='sinhVienEmail' name='sinhVienEmail' value={sinhVienEmail} onInput={this.validSinhVienChange}/>
+                                <p className='text text-danger' >{this.state.errors.sinhVienEmail}</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className='card-footer'>
-                    <button className='btn btn-success' type='submit'>Create</button>
-                    <button className='btn btn-success' type='button'>Update</button>
+                    <button className='btn btn-success mx-3' type='submit'>Thêm sinh viên</button>
+                    <button className='btn btn-success' type='button' onClick={() => {
+                        //Lấy hàm update state từ component cha truyền vào con
+                        let {updateSinhVien} = this.props;
+                        //Gửi ra dữ liệu sau khi thay đổi product
+                        updateSinhVien({...this.state.values});
+                    }}>Cập nhật sinh viên</button>
                 </div>
             </form>
         )
--- src/ReactForm/ReactForm.jsx
+++ src/ReactForm/ReactForm.jsx
@@ -2,10 +2,95 @@ import React, { Component } from 'react'
 import CreateForm from './CreateForm'
 
 export default class ReactForm extends Component {
+
+    state = {
+        arrSinhVien: [
+            {sinhVienId: '01', sinhVienName: 'Ngô Đông Hải', sinhVienPhone: '0327977111', sinhVienEmail: 'haingodong1998@gmail.com'},
+            {sinhVienId: '02', sinhVienName: 'Ngô Việt Anh', sinhVienPhone: '0909737898', sinhVienEmail: 'anhngoviet2003@gmail.com'},
+            {sinhVienId: '03', sinhVienName: 'Nguyễn Nhung', sinhVienPhone: '0775511751', sinhVienEmail: 'nhungnguyen@gmail.com'},
+        ],
+        editSinhVien: {
+            sinhVienId: '',
+            sinhVienName: '',
+            sinhVienPhone: '',
+            sinhVienEmail: '',
+        },
+    }
+
+    addSinhVien = (sinhVienInfo) => {
+        console.log(sinhVienInfo);
+
+        this.state.arrSinhVien.push(sinhVienInfo);
+        this.setState({
+            arrSinhVien: this.state.arrSinhVien,
+        })
+    }
+
+    delSinhVien = (sinhVienIdXoa) => {
+        let sinhVienXoa = this.state.arrSinhVien.findIndex(sinhVien => sinhVien.sinhVienId === sinhVienIdXoa)
+        if (sinhVienXoa !== -1) {
+            this.state.arrSinhVien.splice(sinhVienXoa, 1);
+        }
+
+        // Nhớ setState lại, không là sẽ không render.
+        this.setState({
+            arrSinhVien: this.state.arrSinhVien,
+        })
+    }
+
+    updateSinhVien = (newSinhVien) => {
+        let sinhVien = this.state.arrSinhVien.find(sinhVien => sinhVien.sinhVienId === newSinhVien.sinhVienId);
+        if (sinhVien) {
+            for (let key in sinhVien) {
+                sinhVien[key] = newSinhVien[key];
+            }
+        }
+
+        // Nhớ setState lại, không là sẽ không render.
+        this.setState({
+            arrSinhVien: this.setState.arrSinhVien,
+        })
+    }
+
     render() {
         return (
             <div className='container'>
-                <CreateForm />
+                <CreateForm  addSinhVien={this.addSinhVien} editSinhVien={this.state.editSinhVien} updateSinhVien={this.updateSinhVien}/>
+
+                <table className='table mt-2'>
+                    {/* thead là table header */}
+                    <thead className='bg-dark text-white'>
+                        {/* tr là table row */}
+                        <tr>
+                            {/* th là table header detail */}
+                            <th>Mã sinh viên</th>
+                            <th>Họ tên</th>
+                            <th>Số điện thoại</th>
+                            <th>Email</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.arrSinhVien.map((sinhVien) => {
+                            return <tr key={sinhVien.sinhVienId}>
+                                <td>{sinhVien.sinhVienId}</td>
+                                <td>{sinhVien.sinhVienName}</td>
+                                <td>{sinhVien.sinhVienPhone}</td>
+                                <td>{sinhVien.sinhVienEmail}</td>
+                                <td>
+                                    <button className='btn btn-primary me-2' onClick={() => {
+                                        this.setState({
+                                            editSinhVien: sinhVien,
+                                        })
+                                    }}>Edit</button>
+                                    <button className='btn btn-danger' onClick={() => {
+                                        this.delSinhVien(sinhVien.sinhVienId);
+                                    }}>Del</button>
+                                </td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
             </div>
         )
     }
